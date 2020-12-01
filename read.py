40f694422b11f21f4da24b33292a64a6b6e8728d
ishwar6 <ishawarjangid66@gmail.com>
2020-12-01 20:55:57 +0530

    Videos

--- read.py
+++ read.py
@@ -1,6 +1,15 @@
 
 import cv2 as cv
-img = cv.imread("media/1929.jpg")
+#img = cv.imread("media/1929.jpg")
+# print(type(img))
+#cv.imshow("1929", img)
+capture = cv.VideoCapture('/Users/ishwarjangid/Movies/a.mp4')
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('video', frame)
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
 
-cv.imshow("1929", img)
-cv.waitKey(0)
+capture.release()
+cv.destroyAllWindows()
+# cv.waitKey(0)
